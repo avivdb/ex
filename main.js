b67d58634f60417b5d2d16d47bf9053f31673f85
Aviv Baron David <bardaviv@gmail.com>
2024-05-29 17:49:30 +0300

    Add styling and functionality to ball

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-function onBallClicked() {
-    console.log('hello');
+var gELBall
+function onBallClicked(elBall) {
+
+    var ballHeight = parseInt(elBall.style.height) || 100
+    var ballWidth = parseInt(elBall.style.width) || 100
+    var lineHeight = parseInt(elBall.style.lineHeight) || 100
+
+    ballHeight += 50
+    ballWidth += 50
+    lineHeight += 50
+
+    if (ballHeight > 400) {
+        ballHeight = 100
+        ballWidth = 100
+        lineHeight = 100
+    }
+
+    elBall.innerHTML = ballWidth
+    elBall.style.width = ballWidth + 'px'
+    elBall.style.height = ballHeight + 'px'
+    elBall.style.lineHeight = lineHeight + 'px'
+
+
 }
